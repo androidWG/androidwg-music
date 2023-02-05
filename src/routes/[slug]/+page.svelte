71b3558ab744d6e1ac70d70d24d7ebeff349f159
Sam Rodrigues <sam.roliveira@proton.me>
2023-02-05 07:28:31 -0500

    Added title to release page

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -7,6 +7,10 @@
     let release = data.release;
 </script>
 
+<svelte:head>
+    <title>{release.title} | androidwg music</title>
+</svelte:head>
+
 <Header compact="{true}"></Header>
 
 <Album compact="{false}" title="{release.title}" cover="{release.filename}" links="{release.links}"></Album>
